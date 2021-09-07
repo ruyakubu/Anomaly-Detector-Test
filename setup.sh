8c4fa642fca16453ad705d07d894fa90d63dd954
Ruth Yakubu <ruyakubu@microsoft.com>
2021-09-07 14:51:53 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ echo '------------------------------------------'
 echo 'Creating a Storage Account for the Blob'
 echo $StorageAcctName
 echo $RgName
-az storage account create -n $StorageAcctName --resource-group="${RgName}"
+az storage account create -n $StorageAcctName --resource-group=$RgName
 
 echo 'Storage acount created'
 
