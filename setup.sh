2ef98cdfc88009f2aeebde0235dcee1ba45ae239
Ruth Yakubu <ruyakubu@microsoft.com>
2021-09-09 15:46:03 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,107 +1,95 @@
 #!/bin/bash
 
-# Get Sandbox resource group name
-echo '------------------------------------------'
-echo 'Copy resource group name for the sandbox'
+# Set enviroment variables
 AccountId=$(az account list --query '[0].id'  --output tsv)
 RgName=$(az group list --query '[0].name'  --output tsv)
 Location=$(az group list --query '[0].location'  --output tsv)
-# Location=`az group list --query '[0].location' --output tsv`
-GaLocation=eastus2
 
+GaLocation=eastus2
 StorageAcctName=mylearnstorageacct
+StorageContainerName=learncontainerstorage 
+AzIoTHubName=myLearnIoTHub
+DeviceName=myPowerSensor
+condition='level="storage"'
+endpointName=storageEndpoint
+endpointType=azurestoragecontainer
+routeName=storageRoute
+AdName=learnAnomalyDetector
 
 # Create a Storage Account for the Blob
 echo '------------------------------------------'
-echo 'Creating a Storage Account for the Blob'
-echo $StorageAcctName
-echo $RgName
+echo 'Creating a Storage Account for the Blob...'
 az storage account create -n $StorageAcctName --resource-group="$RgName"
-
 echo 'Storage acount created'
 
 StorageConnStr=$(az storage account show-connection-string -g $RgName  -n $StorageAcctName --query connectionString --output tsv)
 
-StorageContainerName=learncontainerstorage 
-
-
-echo $StorageConnStr
-echo $StorageContainerName
-
 # Create a Storage Container in the Storage Account
 echo '------------------------------------------'
-echo 'Creating a Storage Container in the Storage Account'
+echo 'Creating a Storage Container in the Storage Account...'
 az storage container create \
     --account-name $StorageAcctName \
     --name $StorageContainerName
-    
 echo 'Storage container created'    
-
-AzIoTHubName=myLearnIoTHub
 
 # Create an IoT Hub instance
 echo '------------------------------------------'
-echo 'Creating a IoT Hub instance'
+echo 'Creating a IoT Hub instance...'
 az iot hub create \
     --name $AzIoTHubName \
-    --resource-group $RgName 
-      
+    --resource-group $RgName   
 echo 'IoT Hub created'    
 
 # Create an Azure IoT CLI Extension
 echo '------------------------------------------'
-echo 'Create an Azure IoT CLI Extension'
+echo 'Creating an Azure IoT CLI Extension (OPTIONAL)...'
 az extension add --name azure-iot
-
 az config set extension.use_dynamic_install=yes_without_prompt
-
-DeviceName=myPowerSensor
-
-echo $DeviceName
-echo $AzIoTHubName
+echo 'IoT Hub Azure CLI extension created'  
 
 # Register a device to IoT Hub
 echo '------------------------------------------'
-echo 'Register a device to IoT Hub'
-az iot hub device-identity create --device-id $DeviceName --hub-name $AzIoTHubName
-    
+echo 'Registering a device in IoT Hub...'
+az iot hub device-identity create --device-id $DeviceName --hub-name $AzIoTHubName   
 echo 'IoT Device created' 
 
 IoTConnStr=$(az iot hub connection-string show --query '[0].connectionString'  --output tsv)
 
-
-echo $AccountId
+# Get IoT Hub Device connection string
+echo '------------------------------------------'
+echo 'Copy IOTHUB_DEVICE_CONNECTION_STRING'
+echo '------------------------------------------'
 echo $IoTConnStr
+echo '------------------------------------------'
 
 # Create a destination to Route endpoint IoT messages
 echo '------------------------------------------'
-echo 'Creating a destination to Route IoT messages'
+echo 'Creating a destination to Route IoT messages...'
 az iot hub routing-endpoint create --endpoint-name=S1 --hub-name $AzIoTHubName --endpoint-resource-group $RgName --endpoint-subscription-id $AccountId --endpoint-type azurestoragecontainer --connection-string $StorageConnStr --container $StorageContainerName --encoding=avro
-echo 'IoT routing endpointstorage created' 
-
-condition='level="storage"'
-endpointName="StorageEndpoint"
-endpointType="azurestoragecontainer"
-routeName="StorageRoute"
+echo 'IoT routing endpoint storage created' 
 
 # Create a destination to Route IoT messages
 echo '------------------------------------------'
-echo 'Creating a destination to Route IoT messages'
+echo 'Creating a destination to Route IoT messages...'
 az iot hub route create --name $routeName --hub-name $AzIoTHubName --source devicemessages --resource-group $RgName --endpoint-name=S1 --enabled --condition $condition
 echo 'IoT routing storage created' 
 
 
 # Create an Anomaly Detector instance
 echo '------------------------------------------'
-echo 'Creating an Anomaly Detector instance'
-
-AdName=learnAnomalyDetector
-
+echo 'Creating an Anomaly Detector instance...'
 az cognitiveservices account create --kind AnomalyDetector --name $AdName --resource-group $RgName --location $GaLocation --sku S0 --subscription $AccountId
+echo 'Anomaly Detector instance created' 
 
 APIKey=$(az cognitiveservices account keys list --name $AdName --resource-group $RgName --query key1  --output tsv)
 
+# Get API Key for Anomaly Detector
+echo '------------------------------------------'
+echo 'Copy API_KEY_ANOMALY_DETECTOR'
+echo '------------------------------------------'
 echo $APIKey
+echo '------------------------------------------'
+
 
 echo '--------------------------------------------------------'
 echo '             Resource Setup Completed'
